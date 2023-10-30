e51daa7eecb14a8dde62e908fd34629745e3489a
Claudio Andre Hernandez Zavala <claudiohernandez.zavala@gmail.com>
2023-10-29 22:08:24 -0400

    sdsadsa

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,13 +1,13 @@
 "use client"
 import React from 'react'
 import { SessionProvider } from "next-auth/react"
-
-const Provider = ({children}) => {
+const Provider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <SessionProvider >
         {children}
     </SessionProvider>
   )
-}
+};
+
 
 export default Provider
